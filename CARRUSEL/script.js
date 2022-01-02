f12d97077f57be0bd1d4bb9557df81ddcf9dc0f0
Jacobo Roy <killerpollo22@msn.com>
2022-01-02 11:32:06 -0700

    galeria

---
+++ CARRUSEL/script.js
@@ -0,0 +1,36 @@
+$(function() {
+  var galleryThumbs = new Swiper(".gallery-thumbs", {
+    centeredSlides: true,
+    centeredSlidesBounds: true, 
+    direction: "horizontal",
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: false,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    watchOverflow: true,
+    breakpoints: {
+      480: {
+        direction: "vertical",
+        slidesPerView: 3,
+      }
+    }
+  });
+  var galleryTop = new Swiper(".gallery-top", {
+    direction: "horizontal",
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    },
+  });
+  galleryTop.on('slideChangeTransitionStart', function() {
+    galleryThumbs.slideTo(galleryTop.activeIndex);
+  });
+  galleryThumbs.on('transitionStart', function() {
+    galleryTop.slideTo(galleryThumbs.activeIndex);
+  });
+});
